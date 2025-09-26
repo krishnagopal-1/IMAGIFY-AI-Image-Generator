ac64fff70d60da07c3ad26ac8098f2e1e20b7604
nikku-git <krishnagopal0508@gmail.com>
2025-09-26 22:41:34 +0530

    Added UI

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import Header from '../components/Header'
+import Steps from '../components/Steps'
+import Description from '../components/Description'
+import Testimonials from '../components/Testimonials'
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <Steps />
+      <Description />
+      <Testimonials />
+    </div>
+  )
+}
+
+export default Home
