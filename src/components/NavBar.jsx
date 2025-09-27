1cd99e6eb5b0abcbe618962585e60ee0a22cfd86
nikku-git <krishnagopal0508@gmail.com>
2025-09-27 09:31:52 +0530

    Updated NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -34,7 +34,7 @@ function NavBar() {
                     :
                     <div className='flex items-center gap-2 sm:gap-5'>
                         <p onClick={() => navigate('/buy')} className='cursor-pointer'>Pricing</p>
-                        <button className='bg-zinc-800 text-white px-7 py-2 sm:px-10 text-sm rounded-full'>Login</button>
+                        <button className='bg-zinc-800 text-white px-7 py-2 sm:px-10 text-sm rounded-full cursor-pointer'>Login</button>
                     </div>
                 }
             </div>
