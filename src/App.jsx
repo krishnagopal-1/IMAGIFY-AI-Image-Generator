e4a3b1af5d1c64df8d0988ef9e348c6f53639d26
nikku-git <krishnagopal0508@gmail.com>
2025-09-27 10:22:36 +0530

    Added Footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import Result from './pages/Result';
 import BuyCredit from './pages/BuyCredit';
 import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -14,9 +15,7 @@ function App() {
         <Route path='/result' element={<Result/>} />
         <Route path='/buy' element={<BuyCredit/>} />
       </Routes>
-      
-      
-      
+      <Footer />
     </div>
   )
 }
