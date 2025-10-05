dc6909ec4ed2cd79106b0ea1acd59840b3e2f153
nikku-git <krishnagopal0508@gmail.com>
2025-10-05 19:17:16 +0530

    Added Animations

--- src/components/GenerateBtn.jsx
+++ src/components/GenerateBtn.jsx
@@ -1,8 +1,15 @@
 import { assets } from '../assets/assets';
+import { motion } from "motion/react";
 
 function GenerateBtn() {
   return (
-    <div className='pb-16 text-center'>
+    <motion.div
+      initial={{ opacity: 0.2, y: 100 }}
+      transition={{ duration: 1 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      className='pb-16 text-center'
+    >
       <h1 className='text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold text-neutral-800 py-6 md:py-16'>
         See the magic. Try now
       </h1>
@@ -11,7 +18,7 @@ function GenerateBtn() {
         Generate Images
         <img className='h-6' src={assets.star_group} alt="" />
       </button>
-    </div>
+    </motion.div>
   )
 }
 
